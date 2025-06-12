6b7f288643e8043b14afb8ff19e777c9e630edab
Devatheertha05 <achudeva2005@gmail.com>
2025-06-12 16:16:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,5 +47,4 @@ def predict():
     return jsonify({'prediction': current_prediction})
 
 if __name__ == '__main__':
-       app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
-
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
